4fba12aefc3d573a2f0e7fb4b858ad947cedd208
artemkvshinov <79365581+artemkvshinov@users.noreply.github.com>
2022-12-23 23:35:37 +0500

    Add files via upload

--- valya_podarok/valya_podarok.ino
+++ valya_podarok/valya_podarok.ino
@@ -14,9 +14,8 @@
 #define kol_size 9                    //количество режимов -1
 #define CLK 4
 #define DT 3
-#define SW 2
 #include "GyverEncoder.h"
-Encoder enc1(CLK, DT, SW);
+Encoder enc1(CLK, DT);
 #define Led_Red 11
 #define Led_Green 10
 #define Led_Blue 9
@@ -33,47 +32,82 @@ void setup() {
   TCCR2B = 0b00000001;  // x1
   TCCR2A = 0b00000011;  // fast pwm
   pinMode(button, INPUT_PULLUP);
+  pinMode(Led_Red, OUTPUT);
+  pinMode(Led_Green, OUTPUT);
+  pinMode(Led_Blue, OUTPUT);
 }
 
 void loop() {
+  static uint8_t value;
+  static uint8_t special;
   static uint8_t mode;
   static uint8_t brightness = 125;
   if (button_click(button))(mode == kol_size) ? mode = 0 : mode++;
-  Serial.println(mode);
-  /*
-    switch (mode) {
+  enc1.tick();
+  if (enc1.isRight())  value++;        // если был поворот направо, увеличиваем на 1
+  if (enc1.isLeft())   value--;        // если был поворот налево, уменьшаем на 1
+  if (enc1.isFastR()) value += 10;     // если был быстрый поворот направо, увеличиваем на 10
+  if (enc1.isFastL()) value -= 10;     // если был быстрый поворот налево, уменьшаем на 10
+  if (enc1.isRightH()) special++;      // если было удержание + поворот
+  if (enc1.isLeftH())  special--;
+  Serial.println(value);
+  switch (mode) {
     case 0:
-      Serial.println("Led_OFF");
+      //      Serial.println("Led_OFF");
+      digitalWrite(Led_Red, HIGH);
+      digitalWrite(Led_Green, HIGH);
+      digitalWrite(Led_Blue, HIGH);
       break;
     case 1:
-      Serial.println("RGB_Ring");
+      //      Serial.println("RGB_Ring");
       break;
     case 2:
-      Serial.println("RGB_auto_Ring");
+      //      Serial.println("RGB_auto_Ring");
       break;
     case 3:
-      Serial.println("White");
+      //      Serial.println("White");
+      analogWrite(Led_Red, value);
+      analogWrite(Led_Green, value);
+      analogWrite(Led_Blue, value);
       break;
     case 4:
-      Serial.println("Red");
+      //      Serial.println("Red");
+      analogWrite(Led_Red, value);
+      digitalWrite(Led_Green, HIGH);
+      digitalWrite(Led_Blue, HIGH);
       break;
     case 5:
-      Serial.println("Green");
+      //      Serial.println("Green");
+      digitalWrite(Led_Red, HIGH);
+      analogWrite(Led_Green, value);
+      digitalWrite(Led_Blue, HIGH);
       break;
     case 6:
-      Serial.println("Blue");
+      //      Serial.println("Blue");
+      digitalWrite(Led_Red, HIGH);
+      digitalWrite(Led_Green, HIGH);
+      analogWrite(Led_Blue, value);
       break;
     case 7:
-      Serial.println("Yellow");
+      //      Serial.println("Yellow");
+      analogWrite(Led_Red, value);
+      digitalWrite(Led_Blue, HIGH);
+      analogWrite(Led_Green, value);
       break;
     case 8:
-      Serial.println("Azure");
+      //      Serial.println("Azure");
+      digitalWrite(Led_Red, HIGH);
+      analogWrite(Led_Blue, value);
+      analogWrite(Led_Green, value);
       break;
     case 9:
-      Serial.println("violet");
+      //      Serial.println("violet");
+      analogWrite(Led_Red, value);
+      analogWrite(Led_Blue, value);
+      digitalWrite(Led_Green, HIGH);
       break;
-    }
-  */
+  }
+
 }
 
 
@@ -94,7 +128,7 @@ bool button_click(uint8_t pin) {
     btnTimer = millis();
     out = 0;
     //Serial.println("release");
-    
+
   }
   return !out;
 }
